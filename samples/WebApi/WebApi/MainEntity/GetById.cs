4881f0e484f1c2aaf19bb665925d77bd4a17549a
Vulthil <alexander@keybalance.dk>
2026-01-27 22:01:55 +0100

    Moved some stuff

---
+++ samples/WebApi/WebApi/MainEntity/GetById.cs
@@ -0,0 +1,25 @@
+﻿using Vulthil.Results;
+using Vulthil.SharedKernel.Api;
+using Vulthil.SharedKernel.Application.Messaging;
+using WebApi.Application.MainEntities.GetById;
+
+namespace WebApi.MainEntity;
+
+public static class GetById
+{
+    public class Endpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("mainentity/{id:guid}", async (ISender sender, Guid id) =>
+            {
+                var query = new GetMainEntityByIdQuery(id);
+                var result = await sender.SendAsync(query);
+                return result.Match(
+                    Results.Ok,
+                    e => e.ToIResult());
+            })
+            .WithName("GetMainEntity");
+        }
+    }
+}
